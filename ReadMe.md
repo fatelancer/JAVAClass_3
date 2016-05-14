17e0f195f07dc780eaae8176e67a23f7e6952fed
fatelancer <fatelancer@icloud.com>
2016-05-14 10:04:00 +0800

    pane

--- ReadMe.md
+++ ReadMe.md
@@ -32,3 +32,28 @@ public class CircleArea extends JFrame implements ActionListener{
 
 ```
 
+## 2. 主界面
+```java
+        CircleArea ca = new CircleArea();
+        ca.setTitle("圆面积的计算");
+        ca.setBounds(400,300,280,150);
+        ca.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ca.setVisible(true);
+```
+## 3. 搭建面板
+```java
+    JPanel p0 = new JPanel();
+    JPanel p1 = new JPanel();
+    JPanel p2 = new JPanel();
+    JPanel p3 = new JPanel();
+
+    JLabel l0 = new JLabel("圆面积计算系统", JLabel.CENTER);
+    JLabel l1 = new JLabel("请输入圆的半径：", JLabel.CENTER);
+    JLabel l2 = new JLabel("圆的面积是：", JLabel.CENTER);
+
+    JTextField t1 = new JTextField(10);
+    JTextField t2 = new JTextField(10);
+
+    JButton b1 = new JButton("计算");
+    JButton b2 = new JButton("清空");
+```
--- src/AreaCalculate/CircleArea.java
+++ src/AreaCalculate/CircleArea.java
@@ -9,6 +9,21 @@ import java.awt.event.ActionListener;
  */
 public class CircleArea extends JFrame implements ActionListener{
 
+    JPanel p0 = new JPanel();
+    JPanel p1 = new JPanel();
+    JPanel p2 = new JPanel();
+    JPanel p3 = new JPanel();
+
+    JLabel l0 = new JLabel("圆面积计算系统", JLabel.CENTER);
+    JLabel l1 = new JLabel("请输入圆的半径：", JLabel.CENTER);
+    JLabel l2 = new JLabel("圆的面积是：", JLabel.CENTER);
+
+    JTextField t1 = new JTextField(10);
+    JTextField t2 = new JTextField(10);
+
+    JButton b1 = new JButton("计算");
+    JButton b2 = new JButton("清空");
+
     public CircleArea(){
 
     }
@@ -17,8 +32,12 @@ public class CircleArea extends JFrame implements ActionListener{
 
     }
 
-    public void main(String [] args){
-
+    public static void main(String [] args){
+        CircleArea ca = new CircleArea();
+        ca.setTitle("圆面积的计算");
+        ca.setBounds(400,300,280,150);
+        ca.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ca.setVisible(true);
     }
 
 }
